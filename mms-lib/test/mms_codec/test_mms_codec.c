654ddb58ba596d643f659acbf38106f420604add
Slava Monich <slava.monich@jolla.com>
2014-04-22 13:59:20 +0300

    [test_mms_codec] Added couple more m-retrieve.conf tests
    Those are interesting PDUs from the actual field tests.
    All the confidential information has been wiped out.

--- mms-lib/test/mms_codec/test_mms_codec.c
+++ mms-lib/test/mms_codec/test_mms_codec.c
@@ -63,6 +63,8 @@ int main(int argc, char* argv[])
         "m-retrieve_3.conf",
         "m-retrieve_4.conf",
         "m-retrieve_5.conf",
+        "m-retrieve_6.conf",
+        "m-retrieve_7.conf",
         "m-notifyresp.ind",
         "m-read-rec.ind",
         "m-send.conf"
