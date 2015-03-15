2526c68714a4022d5fc3ee2ddbe0c153aa22dee6
Slava Monich <slava.monich@jolla.com>
2015-03-15 05:06:40 +0200

    Housekeeping

--- mms-lib/src/mms_task_notification.c
+++ mms-lib/src/mms_task_notification.c
@@ -1,7 +1,6 @@
 /*
  * Copyright (C) 2013-2015 Jolla Ltd.
  * Contact: Slava Monich <slava.monich@jolla.com>
- * All rights reserved.
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License version 2 as
